731d140288ef076ba3eecadc59c9ff61a65bb4d3
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-02-22 19:43:28 +0530

    New Project Adding

---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Window;
+import android.view.WindowManager;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
